92b3acabfc26a77f2e6e646f4491b70e1265f460
ApurbaHasanJ <apurbahasanj@gmail.com>
2024-05-15 17:03:45 +0600

    navbar and view product page issue solved

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -2,19 +2,16 @@ import { BsCart3 } from "react-icons/bs";
 import { LuPhoneCall } from "react-icons/lu";
 import { Link, useLocation } from "react-router-dom";
 import NavItems from "./NavItems";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 import Search from "./Search";
 import { FiMenu } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import AllDepartments from "@/components/AllDepartments";
 import { Button } from "@/components/ui/button";
-import { IoMdMenu } from "react-icons/io";
 
 const Navbar = () => {
   const [showDepartments, setShowDepartments] = useState(false);
-  const [showNavitems, setShowNavitems] = useState(false);
-  const [scroll, setScroll] = useState(false);
   const location = useLocation();
 
   const viewProduct = location.pathname.split("/")[1];
@@ -22,27 +19,6 @@ const Navbar = () => {
 
   // cart quantity
   const selectedItem = useSelector((state) => state?.cart?.selectedItem);
-
-  // Add an event listener to track scroll events
-  useEffect(() => {
-    const handleScroll = () => {
-      // Get the current scroll position
-      const scrollY = window.scrollY || window.pageYOffset;
-
-      if (scrollY > 400) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   // call function
   const handleCallClick = () => {
@@ -54,12 +30,18 @@ const Navbar = () => {
   };
 
   return (
-    <header className={cn(viewProduct === "view-product" ? "bg-white": "md:bg-ghost bg-white max-md:pb-3")}>
+    <header
+      className={cn(
+        viewProduct === "view-product"
+          ? "bg-white"
+          : "md:bg-ghost bg-white max-md:pb-3"
+      )}
+    >
       {/* 1st column */}
       <div>
         <div className="container flex items-center justify-between gap-1 md:gap-10 py-5">
           <h4 className="text-primary">Sellora</h4>
-  
+
           {/* <img
             className="h-10 md:h-14"
             src="https://revel-html.codebasket.xyz/assets/images/logo.png"
@@ -79,7 +61,7 @@ const Navbar = () => {
               </div>
             </div>
             {/* cart */}
-            <Link to="/cart" className="relative">
+            <Link to="/cart" className="relative mr-2">
               <BsCart3 className="text-secondary md:text-3xl text-2xl" />
               <span className="bg-primary text-sm rounded-full h-5 w-5 text-center text-white absolute -right-2 -top-2">
                 {selectedItem}
@@ -101,7 +83,6 @@ const Navbar = () => {
       >
         <div className="container w-full flex items-center gap-5 justify-between">
           <div className="max-w w-fit h-full relative">
-            
             <div
               onClick={() => {
                 window.scrollY >= 768 || setShowDepartments(!showDepartments);
